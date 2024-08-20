4044610e9f63ae5e72ad67ac0650e83b7c91b271
sushil pandey <susilpanday9@gmail.com>
2024-08-20 13:03:23 +0600

    allow every origin header

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const {user} =require('./models/index')
-
+const cors = require('cors')
+app.use(cors())
+// it will allow every origin we can restrict it too
+// app.user(cors(
+// {origin: 'https://http://localhost:3000} 
+/* if you are doing locally you have to change the port number only */
+    
+// ))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 // this need to be done to get the req body in the json format
