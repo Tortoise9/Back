4340ba543e635183864784bf3fca2bce2d761c7d
sushil pandey <susilpanday9@gmail.com>
2024-08-20 09:59:26 +0600

    third

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,6 +1,7 @@
 const {user} = require('../models/userModel')
 // this is table 
 exports.Pregister =async(req,res)=>{
+    res.send("iam post method")
     const email = req.body.email
     const username = req.body.username
     const password = req.body.password
