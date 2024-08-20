84d508bcbf191ad8bd067d5da4562a40a973a4f0
sushil pandey <susilpanday9@gmail.com>
2024-08-20 09:17:58 +0600

    first one

---
+++ config/dbConfig.js
@@ -0,0 +1,13 @@
+module.exports ={
+    HOST :"autorack.proxy.rlwy.net",
+    USER : "root",
+    PASSWORD : "RwhXOJRKbQlDILlTlcMtNoaAPYbjtiFv",
+    DB : "railway",
+    diaect:"mysql",
+    pool:{
+        max:5 ,
+        min:0,
+        acquire:30000,
+        idle:10000,
+    },
+};
