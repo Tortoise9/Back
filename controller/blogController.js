c748c66fc04e5c818d0533f8487527ac3d49572d
sushil pandey <susilpanday9@gmail.com>
2024-08-21 22:09:50 +0600

    added for login too

--- controller/blogController.js
+++ controller/blogController.js
@@ -5,6 +5,9 @@ exports.Pregister =async(req,res)=>{
     const email = req.body.email
     const username = req.body.username
     const password = req.body.password
+    if(!email && !username && !password){
+        req.send("please provide valid password")
+    }
     await users.create({
         email:email,
         password:password,
@@ -15,4 +18,35 @@ exports.Pregister =async(req,res)=>{
 }
 exports.renderRegister = (req,res)=>{
     res.send("register")
+}
+exports.LoginP =async (req,res)=>{
+    const email  = req.body.email
+    const password = req.body.password
+     const one =  await users.findAll({
+        where :{
+            email:email
+        }
+    })
+    if(!email &!password){
+        return res.json({
+            error : "sorry input email and password"
+        })
+    }
+    if(one.length>0){
+        if( password===one[0].password){
+
+            res.json(
+                {status :"okay"}
+            )
+        }
+        else{
+            res.json("please input correct password")
+        }
+    }
+    else{
+        return res.send("user doesn't exist " )
+    }
+}
+exports.login = (req,res)=>{
+    res.send("Login page")
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
-const { Pregister, renderRegister } = require('../controller/blogController')
+const { Pregister, renderRegister, LoginP, login } = require('../controller/blogController')
 const router =require("express").Router()
 
 router.route('/register').post(Pregister).get(renderRegister)
-
+router.route('/login').post(LoginP).get(login)
 
 module.exports =router
